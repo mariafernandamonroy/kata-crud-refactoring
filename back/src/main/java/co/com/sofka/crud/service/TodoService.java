e98f53fa0ece78d84160796f99b1b98771aebbd5
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-17 10:39:59 -0500

    (back) conversions TodosList - TodosListDto

--- back/src/main/java/co/com/sofka/crud/service/TodoService.java
+++ back/src/main/java/co/com/sofka/crud/service/TodoService.java
@@ -1,17 +1,17 @@
 package co.com.sofka.crud.service;
 
 import co.com.sofka.crud.dto.TodoDto;
+import co.com.sofka.crud.dto.TodosListDto;
 import co.com.sofka.crud.entities.Todo;
+import co.com.sofka.crud.entities.TodosList;
 import co.com.sofka.crud.repository.TodoRepository;
+import co.com.sofka.crud.repository.TodosListRepository;
 import co.com.sofka.crud.todoRepository.TodotodoRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -21,6 +21,8 @@ public class TodoService {
 
     @Autowired
     private TodoRepository todoRepository;
+    @Autowired
+    private TodosListRepository todosListRepository;
 
     @Autowired
     private ModelMapper mapper;
@@ -28,6 +30,12 @@ public class TodoService {
     public Iterable<TodoDto> listAllTodos(){
         return StreamSupport
                 .stream(todoRepository.findAll().spliterator(), false)
+                .map(this::mapTodoToDto).collect(Collectors.toSet());
+    }
+
+    public Iterable<TodosListDto> listAllTodosList(){
+        return StreamSupport
+                .stream(todosListRepository.findAll().spliterator(), false)
                 .map(this::mapTodoToDto).collect(Collectors.toSet());
     }
 
@@ -57,5 +65,13 @@ public class TodoService {
         Todo todo = mapper.map(todoDto, Todo.class);
         return todo;
     }
-
+    private TodosListDto mapTodoToDto(TodosList todosList){
+        TodosListDto todosListDto = new TodosListDto();
+        todosListDto = mapper.map(todosList, TodosListDto.class);
+        return todosListDto;
+    }
+    private TodosList mapTodoDtoToEntity(TodosListDto todoListDto){
+        TodosList todosList = mapper.map(todoListDto, TodosList.class);
+        return todosList;
+    }
 }
